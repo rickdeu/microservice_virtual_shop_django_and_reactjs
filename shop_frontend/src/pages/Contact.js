e1250c064c60f305f304c6257d8978a3453fed22
rickdeu <hangaloandre@gmail.com>
2023-07-25 22:49:35 +0100

    insert google maps on contact page

--- shop_frontend/src/pages/Contact.js
+++ shop_frontend/src/pages/Contact.js
@@ -13,7 +13,17 @@ const Contact = () => {
         <div className='container-xxl'>
           <div className='row'>
             <div className='col-12'>
-              <h1>Paste here google map</h1>
+
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d123375.2883636634!2d13.446097352286406!3d-14.910452623229652!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1bbcb44c426d406f%3A0x4fd392bb15a4071b!2sInstituto%20Superior%20Polit%C3%A9cnico%20De%20Huila!5e0!3m2!1spt-PT!2sao!4v1690321551356!5m2!1spt-PT!2sao" 
+              width="600" 
+              height="450" 
+              className='border-0 w-100'
+              allowFullScreen="" 
+              loading="lazy" 
+              referrerPolicy="no-referrer-when-downgrade">
+
+              </iframe>
+
             </div>
             <div className='col-12 mt-5'>
               <div className='contact-inner-wrapper d-flex justify-content-between'>
@@ -50,12 +60,12 @@ const Contact = () => {
                       </li>
                       <li className='mb-3 d-flex gap-15 align-items-center'>
                         <AiOutlinePhone className='fs-5' />
-                        <a  href='tel:244926610909'>+244 926-610-909</a>
+                        <a href='tel:244926610909'>+244 926-610-909</a>
 
                       </li>
                       <li className='mb-3 d-flex gap-15 align-items-center'>
                         <AiOutlineMail className='fs-5' />
-                        <a  href='mailto:edgarlopes@gmail.com'>edgarlopes@gmail.com</a>
+                        <a href='mailto:edgarlopes@gmail.com'>edgarlopes@gmail.com</a>
                       </li>
                       <li className='mb-3 d-flex gap-15 align-items-center'>
                         <AiOutlineInfoCircle className='fs-5' />

7c9810b49b70028104bf63839b44b9c7596921d4
rickdeu <hangaloandre@gmail.com>
2023-08-04 11:04:47 +0100

    add unit test

--- shop_product_service/shop/tests.py
+++ shop_product_service/shop/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.utils.http import urlencode
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from shop.models import Category
+from shop import views
 
-# Create your tests here.
+class CategoryTest(APITestCase):
+    def post_category(self, name, image):
+        url = reverse(views.CategoryList.name)
+        data = {'name': name, 'image': image}
+        response = self.client.post(url, data, format='json')
+        return response
+    def test_post_and_get_category_product(self):
+        """
+        Ensure we can create a new Categoru and then get it
+        """
+        new_category_name = 'Water'
+        new_category_image = ''
+        response = self.post_category(new_category_name, new_category_image)
+        print('PK {0}'.format(Category.objects.get().pk))
+        assert response.status_code == status.HTTP_201_CREATED
+        assert Category.objects.count() == 1
+        assert Category.objects.get().name == new_category_name

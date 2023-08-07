9a8e300a605978aec83f3ac5f3e9adecc19ea033
rickdeu <hangaloandre@gmail.com>
2023-08-07 14:22:06 +0100

    list category in frontend

--- shop_product_service/shop_product_service/settings.py
+++ shop_product_service/shop_product_service/settings.py
@@ -43,11 +43,13 @@ INSTALLED_APPS = [
     'shop',
     # instaled apps
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -136,3 +138,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- shop_product_service/shop_product_service/urls.py
+++ shop_product_service/shop_product_service/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # import apps url
 from shop import urls as shop_url
 
 urlpatterns = [
     path('', include(shop_url)),
     path('admin/', admin.site.urls),
-
 ]
 
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

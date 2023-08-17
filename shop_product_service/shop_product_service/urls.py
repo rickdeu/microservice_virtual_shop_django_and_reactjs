ddef67f884b2b1c1e5eccbe0c116fbf0e1cb38cf
rickdeu <hangaloandre@gmail.com>
2023-08-17 10:46:25 +0100

    fix: add coreapi requirement and fix urls.py

--- shop_product_service/shop_product_service/urls.py
+++ shop_product_service/shop_product_service/urls.py
@@ -7,9 +7,18 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # import apps url
 from shop import urls as shop_url
 
+from rest_framework.documentation import include_docs_urls #api documentation
+
+API_TITLE = 'DRONES API' # new
+API_DESCRIPTION = 'A WEB API FOR DRONES.'
+
+
+
 urlpatterns = [
     path('', include(shop_url)),
     path('admin/', admin.site.urls),
+    path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
+
 ]
 
 urlpatterns += staticfiles_urlpatterns()

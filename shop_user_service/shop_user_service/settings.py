3f3526a5ab3c27b9274822c0c812915fc372571c
rickdeu <hangaloandre@gmail.com>
2023-08-18 18:19:34 +0100

    feat: implement  user service

--- shop_user_service/shop_user_service/settings.py
+++ shop_user_service/shop_user_service/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'shop_user_service.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
